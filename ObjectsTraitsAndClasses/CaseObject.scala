ae447db9dd513266545572f20514c50561ffe6b2
Matt Christensen <mr.christensen@gmail.com>
2024-12-17 15:13:32 -0500

    Create CaseObject.scala

---
+++ ObjectsTraitsAndClasses/CaseObject.scala
@@ -0,0 +1,10 @@
+// https://scastie.scala-lang.org/ehzuNmz0QAyWZxzsLjxCeg
+
+case object Bob {
+  val name: String = "Bob"
+  val age: Int = 42
+}
+
+println(Bob.name)
+println(Bob.age)
+println(Bob)
